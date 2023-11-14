837501f6b0b9840adf2110891666c3c4cae6547e
Rachel Walmsley <rachwalm75@gmail.com>
2023-11-14 14:26:22 +0000

    Fix allowed host by removing http//

--- volunteervillage/settings.py
+++ volunteervillage/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://volunteervillage-8a4d89acc796.herokuapp.com/', '127.0.0.1']
+ALLOWED_HOSTS = ['volunteervillage-8a4d89acc796.herokuapp.com/', '127.0.0.1']
 
 
 # Application definition
