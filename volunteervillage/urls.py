9f084521dfe8749424377e55cd3b0561c4b2a229
Rachel Walmsley <rachwalm75@gmail.com>
2023-11-22 16:50:50 +0000

    Set up allauth and login/logout/signup.html feat

--- volunteervillage/urls.py
+++ volunteervillage/urls.py
@@ -20,6 +20,6 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path("", include("volunteer.urls"), name="volunteer-urls")
-    
+    path("", include("volunteer.urls"), name="volunteer-urls"),
+    path('accounts/', include('allauth.urls')),    
 ]
