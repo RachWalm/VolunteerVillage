4697d7958ee3bb6f1737fd16a4b5ac5e8a3f4816
Rachel Walmsley <rachwalm75@gmail.com>
2023-12-09 12:58:03 +0000

    Add functionality that hides the inputs that are taken from database on forms

--- volunteer/forms.py
+++ volunteer/forms.py
@@ -6,21 +6,26 @@ class ProfileForm(forms.ModelForm):
     class Meta:
         model = VolunteerProfile
         fields = ('user_name', 'fname', 'lname', 'phone', 'profile_picture', 'special_skills_description')
+        widgets = {
+            'user_name': forms.HiddenInput(),
+        }
 
         
 class SkillsForm(forms.ModelForm):
     class Meta:
         model = Skills
         fields = ('user_name', 'name', 'skilled',)
-        # widgets = {
-        #     'skills': forms.Select(
-        #         attrs={
-        #             'class': 'form-control', 'required': True
-        #         })
-        # }
+        widgets = {
+            'user_name': forms.HiddenInput(),
+            'name': forms.HiddenInput(),
+        }
         
         
 class TimeForm(forms.ModelForm):
     class Meta:
         model = TimePeriod
         fields = ('user_name', 'name', 'time_length_hours', 'time_length_days', 'section_of_day', 'day')
+        widgets = {
+            'user_name': forms.HiddenInput(),
+            'name': forms.HiddenInput(),
+        }
