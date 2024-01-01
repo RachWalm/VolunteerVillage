cface365465974f6a002730cfd3f45dee9ba96ec
Rachel Walmsley <rachwalm75@gmail.com>
2024-01-01 12:43:12 +0000

    Add Update charity feature, Remove additional submit buttons throughout

--- charity/urls.py
+++ charity/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("index", views.home, name="index"),
     path("charity/add_charity", views.add_charity, name="add_charity"),
+    path("charity/choose_charity", views.search_charity, name="search_charity"),
+    path("charity/update_charity/<id>", views.edit_charity, name="edit_charity"),
 ]
--- charity/views.py
+++ charity/views.py
@@ -5,28 +5,17 @@ from django.http import HttpResponseRedirect
 from .models import CharityProfile
 from .forms import CharityForm
 
-# Create your views here.
-
 
 def home(request):
     return render(request, 'index.html')
 
+
 def add_charity(request):
     form= CharityForm
     if request.method == 'POST':
-        # form_data_co = {
-        #     'user_name': request.user,
-        # }
-
-        print('test')
         form = CharityForm(request.POST) 
         if form.is_valid(): 
-            print('tesst5')
-            # profile = form.save(commit=False)
-            # profile.user_name = request.user
             form.save()
-            print(form)
-            print('test4')
         else:
             print(form.errors)
         return redirect('dashboard')
@@ -34,6 +23,47 @@ def add_charity(request):
         'form': form,
         }
     return render(request, 'coordinator/add_profile.html', context)
-from django.shortcuts import render
 
-# Create your views here.
+def search_charity(request):
+    if request.method == "POST":
+        searched = request.POST['search']
+        charity_profile_all=CharityProfile.objects.filter(charity_name__icontains=searched).values()
+        context = {
+            'searched':searched,
+            'charity_profile_all':charity_profile_all,
+        }
+        return render(request, 'charity/choose_charity.html', context)
+    else:
+        return render(request, 'charity/choose_charity.html',)
+
+
+
+def edit_charity(request, id):
+    pk_logged_in = request.user.pk
+    co_profile_all=CharityProfile.objects.filter().values() #gives the queryset with all details
+    print(co_profile_all)
+    # which_coordinator(request)
+    co_profile = id #get_object_or_404(CoordinatorProfile, lname="one").id
+    print(co_profile)
+    profile = get_object_or_404(CharityProfile, id=co_profile)
+    if request.method == 'POST':
+        form = CharityForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            return redirect('dashboard')
+    form = CharityForm(instance = profile)
+    
+    context = {
+        'form': form,
+        'profile': profile,
+        'pk_logged_in': pk_logged_in
+    }
+    return render(request, 'coordinator/update_profile.html', context)
+
+def delete_charity(request):
+    pk_logged_in = request.user.pk
+    user = get_object_or_404(User, id=pk_logged_in)
+    user.delete()
+    return redirect('dashboard')
+
+# messages.add_message(request, messages.SUCCESS, 'Comment deleted!')
